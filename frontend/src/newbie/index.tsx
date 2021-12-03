8e3426a9a3e28012fb4b747abe9199cd6495ffd7
Irina P <i.pominova@litebox.ru>
2021-12-03 15:06:15 +0400

    header

--- frontend/src/newbie/index.tsx
+++ frontend/src/newbie/index.tsx
@@ -27,48 +27,54 @@ export default function Newbie() {
         className="section-swiper"
       >
         <SwiperSlide>
-          <Swiper
-            navigation={true}
-            slideToClickedSlide={false}
-            className="task-swiper"
-            slidesPerView={"auto"}
-            centeredSlides={true}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-          >
-            <SwiperSlide>
-              <TaskLine day={1}></TaskLine>
-            </SwiperSlide>
-            <SwiperSlide>
-              <TaskLine day={2}></TaskLine>
-            </SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-          </Swiper>
+          <div>
+            <Swiper
+              navigation={true}
+              slideToClickedSlide={false}
+              className="task-swiper"
+              slidesPerView={"auto"}
+              centeredSlides={true}
+              spaceBetween={30}
+            >
+              <SwiperSlide>
+                <TaskLine day={1}></TaskLine>
+              </SwiperSlide>
+              <SwiperSlide>
+                <TaskLine day={2}></TaskLine>
+              </SwiperSlide>
+              <SwiperSlide></SwiperSlide>
+            </Swiper>            
+          </div>          
         </SwiperSlide>
         <SwiperSlide>
-          <h1>Events</h1>
-          <Swiper
-            slidesPerView={"auto"}
-            centeredSlides={true}
-            spaceBetween={30}
-            pagination={{
-              clickable: true,
-            }}
-            className="mySwiper"
-          >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-          </Swiper>
+          <div>
+            <h1>События</h1>
+            <Swiper
+              slidesPerView={"auto"}
+              centeredSlides={true}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              className="mySwiper"
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+          </div>
         </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <h1>Команда</h1>
+          </div>
+        </SwiperSlide>          
       </Swiper>
     </div>
   );
