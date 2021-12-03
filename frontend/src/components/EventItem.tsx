3be31a9030fa5f284e009c373f8e3baf2017b1c7
Nuven <andrew.nuven@gmail.com>
2021-12-03 21:26:18 +0700

    * fix events

--- frontend/src/components/EventItem.tsx
+++ frontend/src/components/EventItem.tsx
@@ -1,11 +1,37 @@
+import { ScheduleOutlined } from "@material-ui/icons";
 import React from "react";
 
 export default function (props?: any) {
+  function getColor(event: any) {
+    switch (event.status) {
+      case "В процессе":
+        return "text-gold";
+        break;
+      case "Завершено":
+        return "text-green";
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <div className="event-item">
       <div className="event-item_datetime">
         <div className="event-item_datetime_date">12.02</div>
         <div className="event-item_datetime_time">14:50</div>
+        <div className="event-item_status">
+          <div className="event-item_icon">
+            <ScheduleOutlined
+              className={getColor(props.event)}
+              style={{ fontSize: "14px" }}
+            ></ScheduleOutlined>
+          </div>
+          <div
+            className={"event-item_status_title" + " " + getColor(props.event)}
+          >
+            {props.event.status}
+          </div>
+        </div>
       </div>
       <div className="event-item_content">
         <div className="event-item_content_title">{props.event.title}</div>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Menu, MenuItem } from "@mui/material";
+import { Badge, Menu, MenuItem } from "@mui/material";
 import React, { useState } from "react";
 import Swiper from "swiper";
 import EventItem from "./EventItem";
@@ -11,9 +11,12 @@ export default function Header(props: any) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
+    if (events.length > 0) {
+      setAnchorEl(event.currentTarget);
+    }
   };
   const handleClose = () => {
+    setEvents([]);
     setAnchorEl(null);
   };
   return (
@@ -23,7 +26,9 @@ export default function Header(props: any) {
 
       <ul className="menu">
         <li>Задачи</li>
-        <li onClick={(event) => handleClick(event)}>События</li>
+        <Badge className="events-badge" badgeContent={events.length}>
+          <li onClick={(event) => handleClick(event)}>События</li>
+        </Badge>
         <li>Команда</li>
       </ul>
       <div className="bonusTotal">2 220 б.</div>
@@ -35,11 +40,6 @@ export default function Header(props: any) {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={
-          {
-            //   "aria-labelledby": "basic-button",
-          }
-        }
       >
         {events.map((event) => {
           return <EventItem event={event}></EventItem>;
