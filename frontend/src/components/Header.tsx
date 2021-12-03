717956b04f020a3c4130ad9b8fff3532016bffdc
Irina P <i.pominova@litebox.ru>
2021-12-03 16:38:01 +0400

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import Progress from "./Progress";
+import bonus from "./TaskLine/tasks.json";
 
 export default function Header (props: any) {
     return (
         <div className="header">
             <div className="logo"></div>
             <Progress/>
+
+            <ul className="menu">
+                <li>Задачи</li>
+                <li>События</li>
+                <li>Команда</li>
+            </ul>
+            <div className="bonusTotal">2 220 б.</div>
+            <div className="search"></div>
         </div>
     );
   }
--- frontend/src/components/Progress.tsx
+++ frontend/src/components/Progress.tsx
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import tasks from "./TaskLine/tasks.json";
 
 export default function Progress (props: any) {
-    const progress = (all: any) => {
+    const progress = () => {
         let done = 0;
-        
+        let all = 0;
+        tasks.map((day) => {
+            day.tasks.map((task) => {
+                if(task.status == 'success') {
+                    done++;
+                }
+                all++;
+            })            
+        })
+        done = done / all * 100;
         return done;
     }
     
     
     return (
-        <div className="progress">{progress(tasks.length)}
+        <div className="progress">
+            <div style={{width: progress() + '%'}}></div>
         </div>
     );
 }
